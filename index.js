87aa0a7e020c47beb78c7686b95b996d30d48716
jedsada <jt@20scoops.com>
2017-03-29 22:53:28 +0700

    imporve api.

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-var express = require('express');
-var app = express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+ extended: false
+}));
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/pond',function(req,res){
-	return res.send('Pond');
+app.get('/control/:command',function(req,res){
+	return res.send(req.params.command);
 });
+
+app.get('/read/:type',function(req,res){
+	return res.send(req.params.type);
+})
 
 var port = app.get('port')
 app.listen(port, function () {

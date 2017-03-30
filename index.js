c7a818ea2eebe0bacf732d03ae1c9a004756104f
jedsada <jt@20scoops.com>
2017-03-30 13:33:21 +0700

    log body.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/control/:command',function(req,res){
 app.post('/control',function(req,res){
 	var userId = req.param('chatfuel user id', null);
 	sendTextMessage(userId, "เปิดไฟให้แล้วจ้า")
-	console.log(userId)
+	console.log(req.body);
 	return res.send(userId);
 });
 
